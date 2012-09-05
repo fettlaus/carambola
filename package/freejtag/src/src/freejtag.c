64be16700018c78b367c837179123cebeddcc6ed
Arne Wischer <Fettlaus@gmail.com>
2012-09-05 19:39:04 +0200

    add Eclipse-generated Hello-World

---
+++ package/freejtag/src/src/freejtag.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : freejtag.c
+ Author      : Arne Wischer
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hallo FreeJTAG"); /* prints Hallo FreeJTAG */
+	return EXIT_SUCCESS;
+}
