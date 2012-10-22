8306b216f330d3d632237ede70afa6440dffc843
Arne Wischer <Fettlaus@gmail.com>
2012-10-22 16:00:12 +0000

    Add log level, always use critical errors

--- package/freejtag/src/src/freejtag.h
+++ package/freejtag/src/src/freejtag.h
@@ -8,11 +8,14 @@
 #ifndef FREEJTAG_H_
 #define FREEJTAG_H_
 #ifdef DEBUG
-#define ERROR(...) g_error(__VA_ARGS__)
 #define PRINT(...) g_message(__VA_ARGS__)
+#define WARNING(...) g_warning(__VA_ARGS__)
+#define CRITICAL(...) g_critical(__VA_ARGS__)
 #else
-#define ERROR(...)
 #define PRINT(...)
+#define WARNING(...)
+#define CRITICAL(...)
 #endif
+#define ERROR(...) g_error(__VA_ARGS__)
 
 #endif /* FREEJTAG_H_ */
