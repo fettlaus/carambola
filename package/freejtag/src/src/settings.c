8b6329b12f0d896fb73a4510e419d75e5709d726
Arne Wischer <Fettlaus@gmail.com>
2012-12-07 03:04:30 +0000

    add sanity checks

--- package/freejtag/src/src/settings.c
+++ package/freejtag/src/src/settings.c
@@ -8,19 +8,67 @@
 #include "settings.h"
 #include <glib.h>
 
-GKeyFile* settings;
+GKeyFile* fj_settings_file;
+FJ_settings* settings;
+GMutex* gm;
+
+void fj_settings_conf_init();
+void fj_settings_conf_free();
 
 gboolean fj_load_settings(GError** error){
-	settings = g_key_file_new();
+	fj_settings_file = g_key_file_new();
+	gm = g_mutex_new();
 #ifdef DEBUG
 	gchar *conffile = "conf/freejtag.cfg";
 #else
 	gchar *conffile = g_strconcat(SYSCONFDIR,"/",PACKAGE,".cfg",NULL);
 #endif
 	PRINT("Loading config from %s",conffile);
-	g_key_file_load_from_file(settings,conffile,G_KEY_FILE_NONE,error);
+	g_key_file_load_from_file(fj_settings_file,conffile,G_KEY_FILE_NONE,error);
 	if(*error!=NULL){
 		return FALSE;
 	}
+	fj_settings_check(error);
+	if(*error!=NULL){
+		return FALSE;
+	}
+	PRINT("Config Sanity Check passed");
+	fj_settings_conf_init(settings);
 	return TRUE;
+}
+
+void fj_settings_conf_init(){
+	settings = g_new(FJ_settings, 1);
+	settings->main = g_new(FJ_settings_main, 1);
+	settings->serial = g_new(FJ_settings_serial, 1);
+	settings->server = g_new(FJ_settings_server, 1);
+	settings->telnet = g_new(FJ_settings_telnet, 1);
+}
+
+void fj_settings_conf_free(){
+	g_free(settings->main);
+	g_free(settings->serial);
+	g_free(settings->server);
+	g_free(settings->telnet);
+	g_free(settings);
+}
+
+void fj_settings_check(GError** error){
+	if(!g_key_file_get_boolean(fj_settings_file,"main","daemonize",error)) return;
+	if(!g_key_file_get_boolean(fj_settings_file,"serial","enabled",error)) return;
+	if(!g_key_file_get_string(fj_settings_file,"serial","dev",error)) return;
+	if(!g_key_file_get_integer(fj_settings_file,"serial","baud",error)) return;
+	if(!g_key_file_get_string(fj_settings_file,"serial","flags",error)) return;
+	if(!g_key_file_get_boolean(fj_settings_file,"server","enabled",error)) return;
+	if(!g_key_file_get_integer(fj_settings_file,"server","port",error)) return;
+	if(!g_key_file_get_boolean(fj_settings_file,"telnet","enabled",error)) return;
+}
+
+void* fj_settings_get(int type, char* group, char* key){
+	if(group == "main"){
+		if(key == "debug") return &settings->main->debug;
+	}else if(group == "serial"){
+		if (key == "enabled") return &settings->serial->enabled;
+	}
+	return NULL;
 }
--- package/freejtag/src/src/settings.h
+++ package/freejtag/src/src/settings.h
@@ -10,12 +10,54 @@
 
 #include <glib.h>
 
-extern GKeyFile* settings;
+extern GKeyFile* fj_settings_file;
 
-#define FJ_SETTING_STRING(cat,key,error) g_key_file_get_string(settings,cat,key,error)
-#define FJ_SETTING_INT(cat,key,error) g_key_file_get_integer(settings,cat,key,error)
-#define FJ_SETTING_BOOL(cat,key,error) g_key_file_get_boolean(settings,cat,key,error)
+typedef struct {
+	gboolean debug;
+} FJ_settings_main;
 
+typedef struct {
+	gboolean enabled;
+	GString dev;
+	gint baud;
+	GString flags;
+} FJ_settings_serial;
+
+typedef struct{
+	gboolean enabled;
+	gint port;
+} FJ_settings_server;
+
+typedef struct {
+	gboolean enabled;
+	gint port;
+} FJ_settings_telnet;
+
+typedef struct {
+	FJ_settings_main* main;
+	FJ_settings_serial* serial;
+	FJ_settings_server* server;
+	FJ_settings_telnet* telnet;
+} FJ_settings;
+
+#define FJ_TYPE_BOOL 0
+#define FJ_TYPE_INT 1
+#define FJ_TYPE_STRING 2
+
+void* fj_settings_get(int type,char* group, char* key);
+void fj_settings_parse();
+void fj_settings_check(GError** error);
+#define FJ_SETTING_STRING(cat,key,error) g_key_file_get_string(FJ_TYPE_STRING,settings,cat,key,error)
+#define FJ_SETTING_INT(cat,key,error) g_key_file_get_integer(FJ_TYPE_INT,settings,cat,key,error)
+#define FJ_SETTING_BOOL(cat,key,error) g_key_file_get_boolean(FJ_TYPE_BOOL,settings,cat,key,error)
+
+#define fj_settings_get_n(type,cat,key) { \
+	g_mutex_lock(gm); \
+	g_key_file_get_##type##(settings,cat,key,NULL); \
+	g_mutex_release(gm); \
+}while(0){}
+
+#define fj_settings_get_bool(cat,key) *((gboolean*) fj_settings_get(FJ_TYPE_BOOL,cat,key))
 
 gboolean fj_load_settings(GError** error);
 
