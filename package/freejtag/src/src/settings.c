f2f77b3e1d187acd258d81861fd6a401ac08b0f2
Arne Wischer <Fettlaus@gmail.com>
2012-12-10 17:40:22 +0000

    settings file saveable

--- package/freejtag/src/src/settings.c
+++ package/freejtag/src/src/settings.c
@@ -9,22 +9,23 @@
 #include <glib.h>
 
 GKeyFile* fj_settings_file;
-FJ_settings* settings;
 GMutex* gm;
 
-void fj_settings_conf_init();
-void fj_settings_conf_free();
+gchar* fj_setting_get_filename(){
+#ifdef DEBUG
+	return "conf/freejtag.cfg";
+#else
+	return g_strconcat(SYSCONFDIR,"/",PACKAGE,".cfg",NULL);
+#endif
+}
 
 gboolean fj_load_settings(GError** error){
+	gchar* file;
+	file = fj_setting_get_filename();
 	fj_settings_file = g_key_file_new();
 	gm = g_mutex_new();
-#ifdef DEBUG
-	gchar *conffile = "conf/freejtag.cfg";
-#else
-	gchar *conffile = g_strconcat(SYSCONFDIR,"/",PACKAGE,".cfg",NULL);
-#endif
-	PRINT("Loading config from %s",conffile);
-	g_key_file_load_from_file(fj_settings_file,conffile,G_KEY_FILE_NONE,error);
+	PRINT("Loading config from %s",file);
+	g_key_file_load_from_file(fj_settings_file,file,G_KEY_FILE_NONE,error);
 	if(*error!=NULL){
 		return FALSE;
 	}
@@ -33,24 +34,7 @@ gboolean fj_load_settings(GError** error){
 		return FALSE;
 	}
 	PRINT("Config Sanity Check passed");
-	fj_settings_conf_init(settings);
 	return TRUE;
-}
-
-void fj_settings_conf_init(){
-	settings = g_new(FJ_settings, 1);
-	settings->main = g_new(FJ_settings_main, 1);
-	settings->serial = g_new(FJ_settings_serial, 1);
-	settings->server = g_new(FJ_settings_server, 1);
-	settings->telnet = g_new(FJ_settings_telnet, 1);
-}
-
-void fj_settings_conf_free(){
-	g_free(settings->main);
-	g_free(settings->serial);
-	g_free(settings->server);
-	g_free(settings->telnet);
-	g_free(settings);
 }
 
 void fj_settings_check(GError** error){
@@ -64,11 +48,9 @@ void fj_settings_check(GError** error){
 	if(!g_key_file_get_boolean(fj_settings_file,"telnet","enabled",error)) return;
 }
 
-void* fj_settings_get(int type, char* group, char* key){
-	if(group == "main"){
-		if(key == "debug") return &settings->main->debug;
-	}else if(group == "serial"){
-		if (key == "enabled") return &settings->serial->enabled;
-	}
-	return NULL;
+void fj_settings_save(GError** error){
+	gchar* cont;
+	gsize length;
+	cont = g_key_file_to_data(fj_settings_file,&length,NULL);
+	g_file_set_contents(fj_setting_get_filename(),cont,length,error);
 }
--- package/freejtag/src/src/settings.h
+++ package/freejtag/src/src/settings.h
@@ -12,44 +12,13 @@
 
 extern GKeyFile* fj_settings_file;
 
-typedef struct {
-	gboolean debug;
-} FJ_settings_main;
-
-typedef struct {
-	gboolean enabled;
-	GString dev;
-	gint baud;
-	GString flags;
-} FJ_settings_serial;
-
-typedef struct{
-	gboolean enabled;
-	gint port;
-} FJ_settings_server;
-
-typedef struct {
-	gboolean enabled;
-	gint port;
-} FJ_settings_telnet;
-
-typedef struct {
-	FJ_settings_main* main;
-	FJ_settings_serial* serial;
-	FJ_settings_server* server;
-	FJ_settings_telnet* telnet;
-} FJ_settings;
-
 #define FJ_TYPE_BOOL 0
 #define FJ_TYPE_INT 1
 #define FJ_TYPE_STRING 2
 
 void* fj_settings_get(int type,char* group, char* key);
-void fj_settings_parse();
 void fj_settings_check(GError** error);
-#define FJ_SETTING_STRING(cat,key,error) g_key_file_get_string(FJ_TYPE_STRING,settings,cat,key,error)
-#define FJ_SETTING_INT(cat,key,error) g_key_file_get_integer(FJ_TYPE_INT,settings,cat,key,error)
-#define FJ_SETTING_BOOL(cat,key,error) g_key_file_get_boolean(FJ_TYPE_BOOL,settings,cat,key,error)
+void fj_settings_save(GError** error);
 
 #define fj_settings_get_n(type,cat,key) { \
 	g_mutex_lock(gm); \
