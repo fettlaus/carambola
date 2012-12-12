7b7d82fe5f1d25fd07a9081380924139421b6088
Arne Wischer <Fettlaus@gmail.com>
2012-12-12 21:44:42 +0000

    added settings tests

--- package/freejtag/src/tests/test_settings.c
+++ package/freejtag/src/tests/test_settings.c
@@ -6,9 +6,19 @@
  */
 #include <glib.h>
 #include "test_settings.h"
-//#include "../src/settings.h"
+#include "../src/settings.h"
 
 void test_settings_init(void){
-	//fj_setting_get_filename();
+	fj_setting_get_filename();
 	g_assert(1==1);
+}
+
+void test_settings_toggle(void){
+	GError *err;
+	g_key_file_set_boolean(fj_settings_file,"main","daemonize",FALSE);
+	g_assert(!g_key_file_get_boolean(fj_settings_file,"main","daemonize",&err));
+	g_assert_no_error(err);
+	g_key_file_set_boolean(fj_settings_file,"main","daemonize",TRUE);
+	g_assert(g_key_file_get_boolean(fj_settings_file,"main","daemonize",&err));
+	g_assert_no_error(err);
 }
