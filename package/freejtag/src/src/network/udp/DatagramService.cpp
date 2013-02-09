5e5b86de140bd5476ec3662d09a5ca7e1ddf90a7
Arne Wischer <Fettlaus@gmail.com>
2013-02-09 02:19:03 +0000

    DatagramService now functional (but ugly)

--- package/freejtag/src/src/network/udp/DatagramService.cpp
+++ package/freejtag/src/src/network/udp/DatagramService.cpp
@@ -5,13 +5,109 @@
  */
 
 #include "DatagramService.h"
+
 #include <common/TimeKeeper.h>
+#include <debug.h>
+
+#include <boost/bind.hpp>
+#include <boost/asio/placeholders.hpp>
 
 namespace freejtag {
 
-DatagramService::DatagramService(boost::asio::io_service& io_service, settings& settings) {
+DatagramService::DatagramService(boost::asio::io_service& io_service, settings& settings):
+    socket_(io_service),
+    udp_handler_(boost::bind(&DatagramService::start_socket,this)) {
     ///< @todo Auto-generated constructor stub
-    TimeKeeper::time();
+    unsigned int this_port = settings.get_value<uint16_t>("port");
+    ip::udp::endpoint ep(ip::udp::v4(),this_port);
+    socket_.open(ep.protocol());
+    socket_.bind(ep);
+
+}
+
+void DatagramService::start_socket(){
+    while(true){
+        cur_message_ = Message::create_message();
+        try{
+            socket_.receive_from(buffer(cur_message_->get_header(),cur_message_->header_length),
+                    sender_endpoint_);
+            PRINT("Got PING");
+            t2 = TimeKeeper::time();
+            cur_message_->decode_header();
+            if(cur_message_->get_type() != PING){
+                continue;
+            }
+            t1 = microseconds(cur_message_->get_timestamp());
+            Message::pointer msg = Message::create_message(PONG,"",TimeKeeper::time().count());
+            std::vector<const_buffer> buf = cur_message_->to_buffers();
+            t3 = TimeKeeper::time();
+            socket_.send_to(msg->to_buffers(),
+                            sender_endpoint_);
+            PRINT("Sent PONG");
+            socket_.receive_from(buffer(cur_message_->get_header(),cur_message_->header_length),
+                                sender_endpoint_);
+            cur_message_->decode_header();
+            if(cur_message_->get_type() != STIM){
+                continue;
+            }
+            PRINT("Got STIM");
+            t4 = microseconds(cur_message_->get_timestamp());
+            microseconds delay((t2-t1-t4+t3)/2);
+            PRINT(delay);
+
+        }catch(boost::system::system_error& err){
+            PRINT("UDP-Error");
+            continue;
+        }
+    }
+
+
+    /*
+     * , boost::bind(&DatagramService::handle_message,this,placeholders::error)
+     */
+}
+
+void DatagramService::handle_message(const boost::system::system_error& err){
+    t2_prev = t2;
+    t2 = TimeKeeper::time();
+    // timestamp
+    if (!err.code()) {
+        cur_message_->decode_header();
+        if(cur_message_->get_type() == PING){
+
+            // construct PONG
+            Message::pointer msg = Message::create_message(PONG,"",TimeKeeper::time().count());
+            //boost::asio::const_buffer& buf(msg->to_buffers());
+            //buf = ;
+            t3 = TimeKeeper::time();
+            socket_.send_to(msg->to_buffers(),
+                sender_endpoint_);
+            /*
+            boost::bind(&DatagramService::handle_send,
+                                this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred,
+                                msg)
+            */
+            // timestamp
+
+            // send PONG
+        }else if(cur_message_->get_type() == STIM){
+            // check timeouts
+            // calc difference
+            // adjust TimeKeeper
+        }
+
+    }else{
+        // TODO reset socket
+    }
+
+    socket_.async_receive_from(buffer(cur_message_->get_header(),cur_message_->header_length),
+        sender_endpoint_,
+        boost::bind(&DatagramService::handle_message,this,placeholders::error));
+}
+
+void DatagramService::handle_send(const boost::system::system_error& err, size_t bytes, Message::pointer msg){
 
 }
 
--- package/freejtag/src/src/network/udp/DatagramService.h
+++ package/freejtag/src/src/network/udp/DatagramService.h
@@ -7,18 +7,38 @@
 #ifndef DATAGRAMSERVICE_H_
 #define DATAGRAMSERVICE_H_
 
+#include <network/Message.h>
+
+#include <boost/thread.hpp>
 #include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/udp.hpp>
+#include <boost/chrono.hpp>
 #include <settings.h>
 
 namespace freejtag {
-
+using namespace boost::asio;
+using namespace boost::chrono;
 /**
  *
  */
 class DatagramService {
 public:
-    DatagramService(boost::asio::io_service& io_service, settings& settings);
+    DatagramService(io_service& io_service, settings& settings);
     virtual ~DatagramService();
+    void start_socket();
+private:
+    void handle_message(const boost::system::system_error& err);
+    void handle_send(const boost::system::system_error& err, size_t bytes, Message::pointer msg);
+    ip::udp::socket socket_;
+    ip::udp::endpoint sender_endpoint_;
+    char data[Message::header_length];
+    boost::thread udp_handler_;
+    Message::pointer cur_message_;
+    microseconds t1;
+    microseconds t2;
+    microseconds t2_prev;
+    microseconds t3;
+    microseconds t4;
 };
 
 } /* namespace freejtag */
