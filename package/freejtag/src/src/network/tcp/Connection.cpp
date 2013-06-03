e02d2178e08b5d8cb77807479a31ba148f9d4e0b
Arne Wischer <Fettlaus@gmail.com>
2013-06-03 21:37:45 +0000

    improved comments in class Connection

--- package/freejtag/src/src/network/tcp/Connection.cpp
+++ package/freejtag/src/src/network/tcp/Connection.cpp
@@ -18,20 +18,23 @@ namespace asio = boost::asio;
 namespace freejtag {
 
 /**
- * Create a new Connection and bind it to a socket.
- * @param service
+ * Create a new Connection and a new socket. It needs an io_service object used for dispatching
+ * and a queue for received Messages.
+ * @param service The io_service object used to dispatch the asynchronous handlers
+ * @param output_queue The NetworkBuffer where received messages will be stored
  */
 Connection::Connection(NetworkBuffer& output_queue, asio::io_service& service) :
     cur_message_(Message::create_message()),
     socket_(service),
-    output_queue_(output_queue),
+    input_buffer_(output_queue),
     strand_(service){
     PRINT("new Connection");
 }
 
 /**
- * Deliver message to this connection.
- * @param msg
+ * Deliver a message to this connection.
+ * @param msg The Message which should be delivered
+ * @pre The socket needs to be connected
  */
 void Connection::deliver(const Message::pointer msg) {
     PRINT("Deliver to Client");
@@ -41,17 +44,19 @@ void Connection::deliver(const Message::pointer msg) {
 }
 
 /**
- * Create new Connection with a socket on service and return a boost::shared_ptr
- * @param service
- * @return boost::shared_ptr
+ * Create new Connection with a socket and return a boost::shared_ptr. After creation the socket
+ * needs to be connected by an acceptor.
+ * @param service The io_service object used to dispatch the asynchronous handlers.
+ * @param output_queue The NetworkBuffer where received messages will be stored.
+ * @return boost::shared_ptr The pointer that should be used to access this connection.
  */
 Connection::pointer Connection::create_new(NetworkBuffer& input_messages, boost::asio::io_service& service) {
     return pointer(new Connection(input_messages, service));
 }
 
 /**
- * Get socket of this connection.
- * @return socket
+ * Get the internally used Socket of this connection.
+ * @return socket The Socket
  */
 asio::ip::tcp::socket& Connection::get_socket() {
 	return socket_;
@@ -59,8 +64,8 @@ asio::ip::tcp::socket& Connection::get_socket() {
 
 /**
  * What to do after we finished writing to the client.
- * @param err error code
- * @param bytes amount of bytes written
+ * @param err Error code
+ * @param bytes Amount of bytes written
  * @throw connectionException If an error occurred during writing
  */
 void Connection::handle_write(const boost::system::system_error& err, size_t bytes, const Message::pointer msg) {
@@ -74,8 +79,9 @@ Connection::~Connection() {
 }
 
 /**
- *  After receiving the header of a Message, this function starts decoding the Message and decides if we should
- *  read any body or if we can push the Massage as-is to the receiving queue and start listening for the next header.
+ *  Handle reading of a message header. After receiving the header of a Message, this function starts
+ *  decoding the Message and decides if we should read any body or if we can push the Massage as-is to
+ *  the receiving queue and start listening for the next header.
  * @param err Error while reading header
  * @throw connection_exception If an error occurred during reading
  */
@@ -89,7 +95,7 @@ void Connection::handle_read_header(const boost::system::system_error& err) {
             strand_.wrap(boost::bind(&Connection::handle_read_body, shared_from_this(), boost::asio::placeholders::error)));
     } else if (length == 0) {
         PRINT(cur_message_ << "received");
-        output_queue_.push(std::make_pair(shared_from_this(), cur_message_));
+        input_buffer_.push(std::make_pair(shared_from_this(), cur_message_));
         cur_message_ = Message::create_message();
         boost::asio::async_read(socket_, boost::asio::buffer(cur_message_->get_header(), cur_message_->header_length),
             strand_.wrap(boost::bind(&Connection::handle_read_header, shared_from_this(), boost::asio::placeholders::error)));
@@ -100,8 +106,8 @@ void Connection::handle_read_header(const boost::system::system_error& err) {
 }
 
 /**
- * After receiving a full Message with body, this function pushes the message to the receiving queue, creates an empty
- *  message and reads a new header into it.
+ * Handle reading of a message body. After receiving a full Message with body, this function
+ * pushes the message to the receiving queue, creates an empty message and reads a new header into it.
  * @param err Error while reading body
  * @throw connection_exception If an error occurred during reading
  */
@@ -110,7 +116,7 @@ void Connection::handle_read_body(const boost::system::system_error& err) {
         throw connection_exception() << connection_info(shared_from_this());
     }
     PRINT(cur_message_ << "received");
-    output_queue_.push(std::make_pair(shared_from_this(), cur_message_));
+    input_buffer_.push(std::make_pair(shared_from_this(), cur_message_));
     cur_message_ = Message::create_message();
     boost::asio::async_read(socket_, boost::asio::buffer(cur_message_->get_header(), cur_message_->header_length),
         strand_.wrap(boost::bind(&Connection::handle_read_header, shared_from_this(), boost::asio::placeholders::error)));
@@ -119,6 +125,7 @@ void Connection::handle_read_body(const boost::system::system_error& err) {
 
 /**
  * Start the Connection. Has to be called after acquiring a connection or we won't be able to handle input and output.
+ * @pre The socket needs to be connected
  */
 void Connection::start() {
     socket_.set_option(boost::asio::ip::tcp::no_delay(true));
@@ -126,6 +133,11 @@ void Connection::start() {
         strand_.wrap(boost::bind(&Connection::handle_read_header, shared_from_this(), boost::asio::placeholders::error)));
 }
 
+/**
+ * Close the Connection. This shuts the socket down for both directions (to close it gracefully)
+ * and closes it afterwards. Async operations should also end after these calls.
+ * @todo Handle the possible close error and closing of async operations.
+ */
 void Connection::close() {
      boost::system::error_code shutdown_error, close_error;
     socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, shutdown_error);
--- package/freejtag/src/src/network/tcp/Connection.h
+++ package/freejtag/src/src/network/tcp/Connection.h
@@ -18,20 +18,27 @@
 
 namespace freejtag {
 
+/**
+ * Class for a single Connection. An object of this class can be created by calling create_new().
+ * This call returns a Connection::pointer which is used to access the object. After creation the
+ * internal socket should be connected by an Acceptor to a single incoming connection. Once it is
+ * connected, the Connection can be made active by Connection::start().
+ */
 class Connection: public MessageTarget, public boost::enable_shared_from_this<Connection> {
 private:
-	boost::asio::ip::tcp::socket socket_; ///< The Socket of this Connection
-    Connection(NetworkBuffer& output_queue, boost::asio::io_service& service); ///< Hidden constructor
-    Message::pointer cur_message_; ///< The message we are reading into
+	boost::asio::ip::tcp::socket socket_; ///< The socket of this Connection
+	Message::pointer cur_message_; ///< The message we are reading into
+    NetworkBuffer& input_buffer_; ///< The buffer for incoming Messages
+    boost::asio::io_service::strand strand_; ///< Strand to synchronize socket access
+
+    Connection(NetworkBuffer& input_buffer, boost::asio::io_service& service);
     void handle_write(const boost::system::system_error& err, size_t, const Message::pointer msg);
     void handle_read_header(const boost::system::system_error& err);
     void handle_read_body(const boost::system::system_error& err);
-    NetworkBuffer& output_queue_;
-    boost::mutex send_mutex_;
-    boost::asio::io_service::strand strand_;
 public:
+    typedef boost::shared_ptr<Connection> pointer; ///< Shared pointer for a connection
+
     ~Connection();
-    typedef boost::shared_ptr<Connection> pointer;
     static pointer create_new(NetworkBuffer& input_messages, boost::asio::io_service& service);
     void deliver(const Message::pointer);
     boost::asio::ip::tcp::socket& get_socket();
