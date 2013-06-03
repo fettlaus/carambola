c824cd636859a4e5c1ea7e112057db5f13e9a258
Arne Wischer <Fettlaus@gmail.com>
2013-06-03 23:12:08 +0000

    merged content of ConnectionBundle into MessageTargetBundle added comments to MessageTargetBundle

--- package/freejtag/src/src/network/tcp/MessageTargetBundle.cpp
+++ package/freejtag/src/src/network/tcp/MessageTargetBundle.cpp
@@ -11,15 +11,44 @@
 
 namespace freejtag {
 
+/**
+ * Send a broadcast. This method calls the deliver() function for every Connection in this bundle.
+ * @param msg Message to deliver
+ * @return Always true (unused)
+ * @todo Catch errors and return error state
+ */
 bool MessageTargetBundle::send_broadcast(const Message::pointer msg) {
     PRINT("Sending Broadcast");
     std::for_each(connections_.begin(),connections_.end(),boost::bind(&Connection::deliver,_1,boost::ref(msg)));
     return true;
 }
 
+/**
+ * Add a new Connection to the bundle. A broadcast will be sent to all Connection instances before
+ * adding the new Connection.
+ * @param conn The new Connection
+ */
 void MessageTargetBundle::add_connection(Connection::pointer conn) {
     this->send_broadcast(Message::create_message(Message::MESS,"New Connection!"));
-    ConnectionBundle::add_connection(conn);
+    connections_.insert(conn);
+}
+
+/**
+ * Remove a single Connection from the bundle. The close() method of the Connection will be called
+ * before removing the Connection.
+ * @param conn The Connection to be removed
+ * @todo Return state of removal (true/false)
+ */
+void MessageTargetBundle::remove_connection(Connection::pointer conn) {
+    conn->close();
+    connections_.erase(conn);
+}
+
+/**
+ * Close all Connection instances. This simply calls close() for every Connection in this bundle.
+ */
+void MessageTargetBundle::close_all_connections() {
+    std::for_each(connections_.begin(),connections_.end(),boost::bind(&Connection::close,_1));
 }
 
 } /* namespace freejtag */
--- package/freejtag/src/src/network/tcp/MessageTargetBundle.h
+++ package/freejtag/src/src/network/tcp/MessageTargetBundle.h
@@ -7,15 +7,25 @@
 #ifndef MESSAGETARGETBUNDLE_H_
 #define MESSAGETARGETBUNDLE_H_
 
-#include "ConnectionBundle.h"
+#include "Connection.h"
+
+#include <set>
 
 namespace freejtag {
 
-class MessageTargetBundle: public freejtag::ConnectionBundle {
+/**
+ *
+ */
+class MessageTargetBundle {
 
 public:
     void add_connection(Connection::pointer conn);
+    void remove_connection(Connection::pointer conn);
+    void close_all_connections();
     bool send_broadcast(const Message::pointer msg);
+protected:
+    typedef std::set<Connection::pointer> ConnectionList; ///< Type of internal storage (set)
+    ConnectionList connections_; ///< Storage for all Connection instances
 };
 
 } /* namespace freejtag */
