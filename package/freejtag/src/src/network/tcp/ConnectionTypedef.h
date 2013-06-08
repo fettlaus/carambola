d2c80ddd5352f10c43570401262c0364e0d753dd
Arne Wischer <Fettlaus@gmail.com>
2013-06-08 19:44:00 +0000

    changed/renamend guards added comments

--- package/freejtag/src/src/network/tcp/ConnectionTypedef.h
+++ package/freejtag/src/src/network/tcp/ConnectionTypedef.h
@@ -4,16 +4,29 @@
  * @author Arne Wischer <Fettlaus@gmail.com>
  */
 
-#ifndef NETWORKSERVICETYPEDEF_H_
-#define NETWORKSERVICETYPEDEF_H_
+#ifndef CONNECTIONTYPEDEF_H_
+#define CONNECTIONTYPEDEF_H_
 
 #include <common/BlockingQueue.h>
 
 namespace freejtag{
 class Connection;
 class Message;
+
+/**
+ * A single Datagram.
+ * This Datagram consists of the shared_ptr to the Connection on which the Message arrived as well as the
+ * shared_ptr to the Message itself.
+ * @see Connection Message
+ */
 typedef std::pair< boost::shared_ptr< Connection >,boost::shared_ptr< Message > > MessageDatagram;
+
+/**
+ * The Buffer used for storing multiple MessageDatagram instances.
+ * It facilitates a BlockingQueue for storage.
+ * @see MessageDatagram BlockingQueue
+ */
 typedef BlockingQueue< MessageDatagram > NetworkBuffer;
 }
 
-#endif /* NETWORKSERVICETYPEDEF_H_ */
+#endif /* CONNECTIONTYPEDEF_H_ */
