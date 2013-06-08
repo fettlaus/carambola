9f87a540be31d2618c0a0bbe0cafd04145171ae1
Arne Wischer <Fettlaus@gmail.com>
2013-06-08 18:32:14 +0000

    renamed guards removed unused bool added comments deprecated one method

--- package/freejtag/src/src/network/tcp/NetworkService.cpp
+++ package/freejtag/src/src/network/tcp/NetworkService.cpp
@@ -8,10 +8,16 @@
 
 namespace freejtag {
 
+/**
+ * Create a new NetworkService instance. This constructor also creates a new acceptor for accepting TCP
+ * connections. It will be connected to a TCPv4 endpoint on the specified port.
+ * @param io_service The connection service handling all asio related functions
+ * @param buffer A buffer in which incoming Messages will be stored
+ * @param port The TCP port on which new connections will be accepted
+ */
 NetworkService::NetworkService(asio::io_service& io_service, NetworkBuffer& buffer, unsigned int port) :
     io_service_(io_service),
     accepto_(io_service_),
-    shutdown_(false),
     input_buffer_(buffer) {
     asio::ip::tcp::endpoint ep(asio::ip::tcp::v4(), port);
     accepto_.open(ep.protocol());
@@ -21,6 +27,11 @@ NetworkService::NetworkService(asio::io_service& io_service, NetworkBuffer& buff
     PRINT("TCP set up ...");
 }
 
+/**
+ * Start accepting new connections. This method creates a new Connection with a new socket first. The
+ * socket of this new Connection will be used asynchronously by the acceptor for connecting a new Client.
+ * Once a new connection is established, the asynchronous accept calls the handle_accept() method.
+ */
 void NetworkService::start_accept() {
     PRINT("Start accept");
     new_connection_ = Connection::create_new(input_buffer_, io_service_);
@@ -28,12 +39,26 @@ void NetworkService::start_accept() {
         boost::bind(&NetworkService::handle_accept, this, new_connection_, asio::placeholders::error));
 }
 
+/**
+ * Closes the acceptor and all Connection instances. This method is mainly used on shutdown of the program.
+ */
 void NetworkService::shutdown(){
     PRINT("Shutting down network");
     accepto_.close();
     connection_bundle_.close_all_connections();
 }
 
+/**
+ * This method gets called by the asynchronous accept. It will be called after a new connection on the port
+ * of the Connection was opened. The method starts the new Connection, delivers a Message to all current
+ * Connection instances and adds the new Connection to the internal storage. A new start_accept() will be
+ * called afterwards.
+ * If an Error occured, no new start_accept() will be called. This will also be called on shutdown of
+ * the acceptor.
+ * @todo Handle errors better
+ * @param ptr The Connection of the newly connected socket
+ * @param err contains an Error if an error occured during accept
+ */
 void NetworkService::handle_accept(Connection::pointer ptr, const boost::system::error_code& err) {
     if (err == 0) {
         PRINT("Incoming Connection!");
@@ -46,16 +71,35 @@ void NetworkService::handle_accept(Connection::pointer ptr, const boost::system:
     }
 }
 
+/**
+ * Send a broadcast to all Connection instances.
+ * @param msg The message to be delivered
+ * @return currently always true
+ * @todo Should return state of success or failure.
+ */
 bool NetworkService::send_broadcast(Message::pointer msg) {
     PRINT("Got Broadcast!");
     connection_bundle_.send_broadcast(msg);
     return true;
 }
 
+/**
+ * Close and remove a single Connection. This method closes a Connection and removes it from the internal
+ * storage. This method should be called, if a connection has to be closed.
+ * @param conn The Connection to be closed
+ */
 void NetworkService::remove_connection(Connection::pointer conn) {
     connection_bundle_.remove_connection(conn);
 }
 
+/**
+ * Send a Message to a given Connection. This method is used to encapsule access to the Connection.
+ * @deprecated Since there are no other measures of encapsulation, this method is deprecated. Also the
+ * returning bool isn't currently used.
+ * @param ptr shared_ptr pointing to the target Connection
+ * @param msg shared_ptr pointing to the Message
+ * @return currently always true. Should return state of success or failure.
+ */
 bool NetworkService::send_message(Connection::pointer ptr, Message::pointer msg) {
     PRINT("Got Direct-Message!");
     ptr->deliver(msg);
--- package/freejtag/src/src/network/tcp/NetworkService.h
+++ package/freejtag/src/src/network/tcp/NetworkService.h
@@ -4,8 +4,8 @@
  * @author Arne Wischer <Fettlaus@gmail.com>
  */
 
-#ifndef TELNET_H_
-#define TELNET_H_
+#ifndef NETWORKSERVICE_H_
+#define NETWORKSERVICE_H_
 
 #include "ConnectionBundle.h"
 
@@ -14,22 +14,31 @@ namespace asio = boost::asio;
 namespace freejtag{
 
 /**
- * http://www.boost.org/doc/libs/1_49_0/doc/html/boost_asio/example/chat/chat_server.cpp
- * http://www.boost.org/doc/libs/1_49_0/doc/html/boost_asio/tutorial/tutdaytime3/src.html
+ * Manages network related TCP functions.
+ * This class is in charge of everything related to the TCP networking. It needs a boost::io_service,
+ * NetworkBuffer and port on startup. The io_service is used to create an acceptor and bind it to a
+ * tcp::endpoint which will be created on the specified port. The method start_accept() needs to be called
+ * to start accepting new connection. A broadcast can be send to all currently connected clients.
+ * <br/>Examples used for creating this class:
+ * <ul>
+ * <li><a href="http://www.boost.org/doc/libs/1_49_0/doc/html/boost_asio/example/chat/chat_server.cpp">
+ * A simple chatserver</a></li>
+ * <li><a href="http://www.boost.org/doc/libs/1_49_0/doc/html/boost_asio/tutorial/tutdaytime3/src.html">
+ * A simple daytime server</a></li></ul>
  */
 class NetworkService{
 private:
-    NetworkBuffer& input_buffer_;
-    asio::io_service& io_service_;
-    asio::ip::tcp::acceptor accepto_;
-    bool shutdown_;
+    NetworkBuffer& input_buffer_; ///< Buffer used to store incoming messages
+    asio::io_service& io_service_; ///< Handles asynchronous networking
+    asio::ip::tcp::acceptor accepto_; ///< Used for incoming connections
+    Connection::pointer new_connection_; ///< Temporary storage for a new Connection
+    MessageTargetBundle connection_bundle_; ///< Stores all current Connection instances
+
     void handle_accept(Connection::pointer ptr, const boost::system::error_code& err);
-    Connection::pointer new_connection_;
-    MessageTargetBundle connection_bundle_;
 public:
+    NetworkService(asio::io_service& io_service, NetworkBuffer& buffer, unsigned int port);
     void start_accept();
     void shutdown();
-    NetworkService(asio::io_service& io_service, NetworkBuffer& buffer, unsigned int port);
     void remove_connection(Connection::pointer conn);
     bool send_broadcast(Message::pointer msg);
     bool send_message(Connection::pointer conn, Message::pointer msg);
@@ -37,4 +46,4 @@ public:
 
 }
 
-#endif /* TELNET_H_ */
+#endif /* NETWORKSERVICE_H_ */
